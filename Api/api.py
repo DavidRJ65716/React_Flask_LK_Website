4fe743dbab17858635e9629cb6ce7175e5b19c55
DavidRJ65716 <daverj65716@gmail.com>
2023-10-24 15:41:36 -0500

    Added Api call to log into database

--- Api/api.py
+++ Api/api.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import time
 from datetime import datetime
+from flask_mysqldb import MySQL
 
-app = Flask(__name__, static_folder="../build", static_url_path='/')
+app = Flask(__name__, static_folder="../client/build", static_url_path='/')
 app.debug = True
 
 @app.route('/')
@@ -17,6 +18,25 @@ def get_current_time():
 def get_current_date():
     return{'date': datetime.now()}
  
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'guess'
+app.config['MYSQL_PASSWORD'] = '4[Wkb0XM-kyhGZLC'
+app.config['MYSQL_DB'] = 'main_website'
+mydb = MySQL(app)
+
+
+@app.route('/Api/dblogin')
+def db_login():
+
+    cur = mydb.connection.cursor()
+    cur.execute("""SELECT * FROM `employee` WHERE 1""")
+    rv = cur.fetchall()
+    print("200 ok\n")
+    print(str(rv))
+
+    return str(rv)
+
+
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
--- Api/database/db.py
+++ Api/database/db.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_mysqldb import MySQL
 
-dbApp = Flask(__name__)
+app = Flask(__name__)
 
-def dbLoggin(database):
-    dbApp.config['MYSQL_HOST'] = 'localhost'
-    dbApp.config['MYSQL_USER'] = ''
-    dbApp.config['MYSQL_PASSWORD'] = ''
-    dbApp.config['MYSQL_DB'] = database
-    mydb = MySQL(dbApp)
-    return mydb
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'guess'
+app.config['MYSQL_PASSWORD'] = '4[Wkb0XM-kyhGZLC'
+app.config['MYSQL_DB'] = 'main_website'
+mydb = MySQL(app)
+
+
+@app.route('/Api/dblogin')
+def db_login():
+    try:
+        cur = mydb.connection.cursor()
+        cur.execute("""SELECT * FROM `employee` WHERE 1""")
+        rv = cur.fetchall()
+        print("200 ok\n")
+        print(str(rv))
+    except:
+        print("Failed to connect to database")
